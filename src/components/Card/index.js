1016cb750de14652b6fa9654cb92c1a7052b3993
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-09-19 18:31:05 -0300

    alterações no card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -6,8 +6,8 @@ function Card({ player }) {
       {player && (
         <div className="card">
           <img className="card__avatar" src={player.avatar} alt="Avatar" />
-          <span className="card__nick">{player.nick}</span>
           <img className="card__flag" src={player.flag} alt="Flag" />
+          <span className="card__nick">{player.nick}</span>
         </div>
       )}
     </>
