8534609877f991873fe92d923aefc80dc844fe7a
Wesley Eugênio <wesleyeugenio3@gmail.com>
2022-09-19 21:49:07 -0300

    definição de todos os jogos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,6 +25,7 @@ function Home() {
     <div className="home">
       <section className="home__column--first">
         <div className="home__group">
+          <h1 className="home__title">Oitavas de Final</h1>
           <div className="home__card">
             <Card player={players[0]} />
             <img className="icon--versus" src={Versus} alt="Versus" />
@@ -51,17 +52,85 @@ function Home() {
       </section>
       <section className="home__column--second">
         <div className="home__group">
+          <h1 className="home__title">Quartas de Final</h1>
           <div className="home__card">
-            <Card player={players[2]} />
+            <Card player={players[1]} />
             <img className="icon--versus" src={Versus} alt="Versus" />
-            <Card player={players[3]} />
+            <Card player={players[2]} />
           </div>
         </div>
         <div className="home__group">
           <div className="home__card">
-            <Card player={players[4]} />
+            <Card player={players[5]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[7]} />
+          </div>
+        </div>
+      </section>
+      <section className="home__column--center">
+        <div className="home__group">
+          <h1 className="home__title">Final</h1>
+          <div className="home__card">
+            <Card player={players[5]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[15]} />
+          </div>
+        </div>
+        <div className="home__group">
+          <h1 className="home__title">SemiFinal</h1>
+          <div className="home__card">
+            <Card player={players[2]} />
             <img className="icon--versus" src={Versus} alt="Versus" />
             <Card player={players[5]} />
+          </div>
+          <div className="home__card">
+            <Card player={players[8]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[15]} />
+          </div>
+        </div>
+      </section>
+      <section className="home__column--second">
+        <div className="home__group">
+          <h1 className="home__title">Quartas de Final</h1>
+          <div className="home__card">
+            <Card player={players[8]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[11]} />
+          </div>
+        </div>
+        <div className="home__group">
+          <div className="home__card">
+            <Card player={players[13]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[15]} />
+          </div>
+        </div>
+      </section>
+      <section className="home__column--first">
+        <div className="home__group">
+          <h1 className="home__title">Oitavas de Final</h1>
+          <div className="home__card">
+            <Card player={players[8]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[9]} />
+          </div>
+          <div className="home__card">
+            <Card player={players[10]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[11]} />
+          </div>
+        </div>
+        <div className="home__group">
+          <div className="home__card">
+            <Card player={players[12]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[13]} />
+          </div>
+          <div className="home__card">
+            <Card player={players[14]} />
+            <img className="icon--versus" src={Versus} alt="Versus" />
+            <Card player={players[15]} />
           </div>
         </div>
       </section>
